bf297616a2625db6e916a76cc8f9634cd487ed1d
Vraj Barot <vraj.barot@vetchip.com.au>
2024-03-20 13:03:32 +0800

    chaged to bar on bottom

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -1,60 +1,4 @@
 -- Status line
--- return {
---   -- https://github.com/nvim-lualine/lualine.nvim
---   "nvim-lualine/lualine.nvim",
---   dependencies = {
---     -- https://github.com/nvim-tree/nvim-web-devicons
---     "nvim-tree/nvim-web-devicons",  -- fancy icons
---     -- https://github.com/linrongbin16/lsp-progress.nvim
---     "linrongbin16/lsp-progress.nvim", -- LSP loading progress
---   },
---   opts = {
---     options = {
---       -- For more themes, see https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
---       theme = "catppuccin", -- "auto, tokyonight, catppuccin, codedark, nord"
---     },
---     sections = {
---       lualine_c = {
---         {
---           -- Customize the filename part of lualine to be parent/filename
---           "filename",
---           file_status = true, -- Displays file status (readonly status, modified status)
---           newfile_status = false, -- Display new file status (new file means no write after created)
---           path = 4,          -- 0: Just the filename
---           -- 1: Relative path
---           -- 2: Absolute path
---           -- 3: Absolute path, with tilde as the home directory
---           -- 4: Filename and parent dir, with tilde as the home directory
---           symbols = {
---             modified = "[+]", -- Text to show when the file is modified.
---             readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
---           },
---         },
---       },
---     },
---     winbar = {
---       lualine_c = {
---         {
---           -- Customize the filename part of lualine to be parent/filename
---           "filename",
---           file_status = true, -- Displays file status (readonly status, modified status)
---           newfile_status = false, -- Display new file status (new file means no write after created)
---           path = 4,          -- 0: Just the filename
---           -- 1: Relative path
---           -- 2: Absolute path
---           -- 3: Absolute path, with tilde as the home directory
---           -- 4: Filename and parent dir, with tilde as the home directory
---           symbols = {
---             modified = "[+]", -- Text to show when the file is modified.
---             readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
---           },
---         },
---       },
---     },
---   },
--- }
-
--- Status line
 return {
 	-- https://github.com/nvim-lualine/lualine.nvim
 	"nvim-lualine/lualine.nvim",
@@ -67,22 +11,27 @@ return {
 	opts = {
 		options = {
 			icons_enabled = true,
-			theme = "catppuccin", -- lualine theme
+			-- theme = "catppuccin", -- lualine theme
+			theme = "tokyonight", -- lualine theme
 			globalstatus = false,
 			component_separators = "|",
-			section_separators = { left = "", right = "" },
+			-- section_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
 		},
-		tabline = {
+		sections = {
 			lualine_a = {
-				{ "mode", separator = { left = "" }, right_padding = 2 },
+				-- { "mode", separator = { left = "" }, right_padding = 2 },
+				{ "mode", separator = { left = "" }, right_padding = 2 },
 			},
 			lualine_b = { "filename" },
 			-- lualine_x = { "buffers" },
 			lualine_y = { "filetype", "progress" },
 			lualine_z = {
-				{ "location", separator = { right = "" }, left_padding = 2 },
+				-- { "location", separator = { right = "" }, left_padding = 2 },
+				{ "location", separator = { right = "" }, left_padding = 2 },
+				{ "location", left_padding = 2 },
 			},
 		},
-		sections = {},
+		tabline = {},
 	},
 }
