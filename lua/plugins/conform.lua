f63eab122d1ff8745a475b91b07480fc72c5f670
Vraj Barot <vraj.barot@vetchip.com.au>
2024-05-15 11:22:34 +0800

    added keymapping

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -17,12 +17,12 @@ return {
 			},
 		})
 
-		-- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-		-- 	conform.format({
-		-- 		lsp_fallback = true,
-		-- 		async = false,
-		-- 		timeout_ms = 1000,
-		-- 	})
-		-- end, { desc = "Format file or range (in visual mode)" })
+		vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range" })
 	end,
 }
