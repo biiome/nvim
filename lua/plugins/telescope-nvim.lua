2c376f7522f03e10af021962b55826c8e29ba94d
Vraj Barot <vraj.barot@vetchip.com.au>
2024-05-15 10:30:56 +0800

    Better configuration

--- lua/plugins/telescope-nvim.lua
+++ lua/plugins/telescope-nvim.lua
@@ -1,28 +1,28 @@
 -- Fuzzy finder
 return {
-	-- https://github.com/nvim-telescope/telescope.nvim
 	"nvim-telescope/telescope.nvim",
-	lazy = true,
 	branch = "0.1.x",
 	dependencies = {
-		-- https://github.com/nvim-lua/plenary.nvim
-		{ "nvim-lua/plenary.nvim" },
-		{
-			-- https://github.com/nvim-telescope/telescope-fzf-native.nvim
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-			cond = function()
-				return vim.fn.executable("make") == 1
-			end,
-		},
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
 	},
-	opts = {
-		defaults = {
-			layout_config = {
-				vertical = {
-					width = 0.75,
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
 				},
 			},
-		},
-	},
+		})
+		telescope.load_extension("fzf")
+	end,
 }
