4b8c572c8cf7c173487ce4918305b68929cb4dda
Vraj Barot <vraj.barot@vetchip.com.au>
2024-05-15 11:05:57 +0800

    Formatting, removed orphaned code

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,146 +1,95 @@
 -- Auto-completion / Snippets
 return {
-  -- https://github.com/hrsh7th/nvim-cmp
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    -- Snippet engine & associated nvim-cmp source
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
+	-- https://github.com/hrsh7th/nvim-cmp
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		-- Snippet engine & associated nvim-cmp source
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
 
-    -- LSP completion capabilities
-    "hrsh7th/cmp-nvim-lsp",
+		-- LSP completion capabilities
+		"hrsh7th/cmp-nvim-lsp",
 
-    -- Additional user-friendly snippets
-    "rafamadriz/friendly-snippets",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
+		-- Additional user-friendly snippets
+		"rafamadriz/friendly-snippets",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
 
-    -- VS-Code like pictograms
-    "onsails/lspkind.nvim",
-  },
-  config = function()
-    local cmp = require("cmp")
+		-- VS-Code like pictograms
+		"onsails/lspkind.nvim",
+	},
+	config = function()
+		local cmp = require("cmp")
 
-    local luasnip = require("luasnip")
+		local luasnip = require("luasnip")
 
-    local lspkind = require("lspkind")
+		local lspkind = require("lspkind")
 
-    local icons = {
-      Array = " ",
-      Boolean = "󰨙 ",
-      Class = " ",
-      Codeium = "󰘦 ",
-      Color = " ",
-      Control = " ",
-      Collapsed = " ",
-      Constant = "󰏿 ",
-      Constructor = " ",
-      Copilot = " ",
-      Enum = " ",
-      EnumMember = " ",
-      Event = " ",
-      Field = " ",
-      File = " ",
-      Folder = " ",
-      Function = "󰊕 ",
-      Interface = " ",
-      Key = " ",
-      Keyword = " ",
-      Method = "󰊕 ",
-      Module = " ",
-      Namespace = "󰦮 ",
-      Null = " ",
-      Number = "󰎠 ",
-      Object = " ",
-      Operator = " ",
-      Package = " ",
-      Property = " ",
-      Reference = " ",
-      Snippet = " ",
-      String = " ",
-      Struct = "󰆼 ",
-      TabNine = "󰏚 ",
-      Text = " ",
-      TypeParameter = " ",
-      Unit = " ",
-      Value = " ",
-      Variable = "󰀫 ",
-    }
+		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+		require("luasnip.loaders.from_vscode").lazy_load()
+		luasnip.config.setup({})
 
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
-    luasnip.config.setup({})
-
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      completion = {
-        completeopt = "menu,menuone,preview,noinsert",
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- scroll backward
-        ["<C-f>"] = cmp.mapping.scroll_docs(4), -- scroll forward
-        ["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        }),
-        -- Tab through suggestions or when a snippet is active, tab to the next argument
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        -- Tab backwards through suggestions or when a snippet is active, tab to the next argument
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" }, -- lsp
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
-      }),
-      window = {
-        -- Add borders to completions popups
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      --    formatting = {
-      -- 	format = function(_, item)
-      -- 		if icons[item.kind] then
-      -- 			item.kind = icons[item.kind] .. item.kind
-      -- 		end
-      -- 		return item
-      -- 	end,
-      -- }
-
-      -- Use lsp-kind icons
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          elipsis_char = "...",
-        }),
-      },
-    })
-  end,
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			completion = {
+				completeopt = "menu,menuone,preview,noinsert",
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-b>"] = cmp.mapping.scroll_docs(-4), -- scroll backward
+				["<C-f>"] = cmp.mapping.scroll_docs(4), -- scroll forward
+				["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
+				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}),
+				-- Tab through suggestions or when a snippet is active, tab to the next argument
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				-- Tab backwards through suggestions or when a snippet is active, tab to the next argument
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" }, -- lsp
+				{ name = "luasnip" }, -- snippets
+				{ name = "buffer" }, -- text within current buffer
+				{ name = "path" }, -- file system paths
+			}),
+			window = {
+				-- Add borders to completions popups
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			-- Use lsp-kind icons
+			formatting = {
+				format = lspkind.cmp_format({
+					maxwidth = 50,
+					elipsis_char = "...",
+				}),
+			},
+		})
+	end,
 }
