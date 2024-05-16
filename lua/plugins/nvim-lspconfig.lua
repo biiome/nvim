f2107145b78673ad470973e0f546056284cc0eb8
Vraj Barot <vraj.barot@vetchip.com.au>
2024-05-16 15:18:56 +0800

    Added restart LSP shortcut

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -62,8 +62,8 @@ return {
 				-- opts.desc = "Show documentation for what is under cursor"
 				-- keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 				--
-				-- opts.desc = "Restart LSP"
-				-- keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				opts.desc = "Restart LSP"
+				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 			end,
 		})
 
