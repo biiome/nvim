27cb942313a1d0c7983df8fa840938d4dff85861
Vraj Barot <vraj.barot@vetchip.com.au>
2024-05-28 16:56:26 +0800

    formatting changes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -89,22 +89,22 @@
 
 -- Kanagawa Theme (Original)
 -- return {
---   "rebelot/kanagawa.nvim", -- You can replace this with your favorite colorscheme
---   lazy = false,           -- We want the colorscheme to load immediately when starting Neovim
---   priority = 1000,        -- Load the colorscheme before other non-lazy-loaded plugins
---   opts = {
---     -- Replace this with your scheme-specific settings or remove to use the defaults
---     -- transparent = true,
---     background = {
---       -- light = "lotus",
---       dark = "wave",
---       -- dark = "dragon",
---     },
---   },
---   config = function(_, opts)
---     require("kanagawa").setup(opts) -- Replace this with your favorite colorscheme
---     vim.cmd("colorscheme kanagawa") -- Replace this with your favorite colorscheme
---   end,
+-- 	"rebelot/kanagawa.nvim", -- You can replace this with your favorite colorscheme
+-- 	lazy = false, -- We want the colorscheme to load immediately when starting Neovim
+-- 	priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
+-- 	opts = {
+-- 		-- Replace this with your scheme-specific settings or remove to use the defaults
+-- 		transparent = true,
+-- 		background = {
+-- 			-- light = "lotus",
+-- 			dark = "wave",
+-- 			-- dark = "dragon",
+-- 		},
+-- 	},
+-- 	config = function(_, opts)
+-- 		require("kanagawa").setup(opts) -- Replace this with your favorite colorscheme
+-- 		vim.cmd("colorscheme kanagawa") -- Replace this with your favorite colorscheme
+-- 	end,
 -- }
 
 -- Tokyo Night Theme
@@ -144,27 +144,28 @@
 
 -- Sonokai Theme
 -- return {
---   -- https://github.com/sainnhe/sonokai
---   'sainnhe/sonokai',
---   lazy = false, -- We want the colorscheme to load immediately when starting Neovim
---   priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
---   config = function(_, opts)
---     vim.g.sonokai_style = "default" -- "default, atlantis, andromeda, shusia, maia, espresso"
---     vim.cmd("colorscheme sonokai") -- Replace this with your favorite colorscheme
---   end
+-- 	-- https://github.com/sainnhe/sonokai
+-- 	"sainnhe/sonokai",
+-- 	lazy = false, -- We want the colorscheme to load immediately when starting Neovim
+-- 	priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
+-- 	config = function(_, opts)
+-- 		vim.g.sonokai_style = "default" -- "default, atlantis, andromeda, shusia, maia, espresso"
+-- 		vim.cmd("colorscheme sonokai") -- Replace this with your favorite colorscheme
+-- 	end,
 -- }
 
 -- One Nord Theme
 -- return {
---   -- https://github.com/rmehri01/onenord.nvim
---   'rmehri01/onenord.nvim',
---   lazy = false, -- We want the colorscheme to load immediately when starting Neovim
---   priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
---   config = function(_, opts)
---     vim.cmd("colorscheme onenord") -- Replace this with your favorite colorscheme
---   end
+-- 	-- https://github.com/rmehri01/onenord.nvim
+-- 	"rmehri01/onenord.nvim",
+-- 	lazy = false, -- We want the colorscheme to load immediately when starting Neovim
+-- 	priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
+-- 	config = function(_, opts)
+-- 		vim.cmd("colorscheme onenord") -- Replace this with your favorite colorscheme
+-- 	end,
 -- }
 
+-- Tokyonight Theme
 return {
 	"folke/tokyonight.nvim",
 	lazy = false,
