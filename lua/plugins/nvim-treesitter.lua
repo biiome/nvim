9822baec45149367ad2b9b8e431e7f7f1fd1e710
Vraj Barot <vraj.barot@vetchip.com.au>
2024-07-10 12:04:39 +0800

    formatting

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,34 +1,35 @@
 -- Code Tree Support / Syntax Highlighting
 return {
-  -- https://github.com/nvim-treesitter/nvim-treesitter
-  "nvim-treesitter/nvim-treesitter",
-  event = "VeryLazy",
-  dependencies = {
-    -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "windwp/nvim-ts-autotag",
-  },
-  build = ":TSUpdate",
-  opts = {
-    highlight = { enable = true }, -- enable syntax highlighting
-    indent = { enable = true },  -- enable indentation
-    autotag = { enable = true }, -- enable autotagging (w/ nbim-ts-autotaf plugin)
-    auto_install = true,         -- automatically install syntax support when entering new file type buffer
-    ensure_installed = {
-      "lua",
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
-      },
-    },
-  },
-  config = function(_, opts)
-    local configs = require("nvim-treesitter.configs")
-    configs.setup(opts)
-  end,
+	-- https://github.com/nvim-treesitter/nvim-treesitter
+	"nvim-treesitter/nvim-treesitter",
+	event = "VeryLazy",
+	dependencies = {
+		-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"windwp/nvim-ts-autotag",
+	},
+	build = ":TSUpdate",
+	opts = {
+		highlight = { enable = true }, -- enable syntax highlighting
+		indent = { enable = true }, -- enable indentation
+		autotag = { enable = true }, -- enable autotagging (w/ nbim-ts-autotaf plugin)
+		auto_install = true, -- automatically install syntax support when entering new file type buffer
+		ensure_installed = {
+			"lua",
+			"python",
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			},
+		},
+	},
+	config = function(_, opts)
+		local configs = require("nvim-treesitter.configs")
+		configs.setup(opts)
+	end,
 }
