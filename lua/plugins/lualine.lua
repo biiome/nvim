f8476adcfd81fea7cc5570f7432b2c9526f61ea5
Vraj Barot <vraj.barot@vetchip.com.au>
2025-02-03 15:41:09 +0800

    new lualine config \

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,230 @@
+local colors = require "oldworld.palette"
+vim.o.laststatus = vim.g.lualine_laststatus
+local modecolor = {
+  n = colors.red,
+  i = colors.cyan,
+  v = colors.purple,
+  -- [""] = colors.purple,
+  V = colors.red,
+  c = colors.yellow,
+  no = colors.red,
+  s = colors.yellow,
+  S = colors.yellow,
+  -- [""] = colors.yellow,
+  ic = colors.yellow,
+  R = colors.green,
+  Rv = colors.purple,
+  cv = colors.red,
+  ce = colors.red,
+  r = colors.cyan,
+  rm = colors.cyan,
+  ["r?"] = colors.cyan,
+  ["!"] = colors.red,
+  t = colors.bright_red,
+}
+
+local theme = {
+  normal = {
+    a = { fg = colors.bg_dark, bg = colors.blue },
+    b = { fg = colors.blue, bg = colors.white },
+    c = { fg = colors.white, bg = colors.bg_dark },
+    z = { fg = colors.white, bg = colors.bg_dark },
+  },
+  insert = { a = { fg = colors.bg_dark, bg = colors.orange } },
+  visual = { a = { fg = colors.bg_dark, bg = colors.green } },
+  replace = { a = { fg = colors.bg_dark, bg = colors.green } },
+}
+
+local space = {
+  function()
+    return " "
+  end,
+  color = { bg = colors.bg_dark, fg = colors.blue },
+}
+
+local filename = {
+  "filename",
+  color = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+  separator = { left = "", right = "" },
+}
+
+local filetype = {
+  "filetype",
+  icons_enabled = false,
+  color = { bg = colors.gray2, fg = colors.blue, gui = "italic,bold" },
+  separator = { left = "", right = "" },
+}
+
+local branch = {
+  "branch",
+  icon = "",
+  color = { bg = colors.green, fg = colors.bg, gui = "bold" },
+  separator = { left = "" },
+}
+
+local location = {
+  "location",
+  color = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+  separator = { left = "", right = "" },
+}
+
+local diff = {
+  "diff",
+  color = { bg = colors.gray2, fg = colors.bg, gui = "bold" },
+  separator = { left = "" },
+  symbols = { added = " ", modified = " ", removed = " " },
+
+  diff_color = {
+    added = { fg = colors.green },
+    modified = { fg = colors.yellow },
+    removed = { fg = colors.red },
+  },
+}
+
+local modes = {
+  "mode",
+  color = function()
+    local mode_color = modecolor
+    return { bg = mode_color[vim.fn.mode()], fg = colors.bg_dark, gui = "bold" }
+  end,
+  separator = { left = "", right = "" },
+}
+
+local function getLspName()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local buf_clients = vim.lsp.get_clients { bufnr = bufnr }
+  local buf_ft = vim.bo.filetype
+  if next(buf_clients) == nil then
+    return "  No servers"
+  end
+  local buf_client_names = {}
+
+  for _, client in pairs(buf_clients) do
+    if client.name ~= "null-ls" then
+      table.insert(buf_client_names, client.name)
+    end
+  end
+
+  local lint_s, lint = pcall(require, "lint")
+  if lint_s then
+    for ft_k, ft_v in pairs(lint.linters_by_ft) do
+      if type(ft_v) == "table" then
+        for _, linter in ipairs(ft_v) do
+          if buf_ft == ft_k then
+            table.insert(buf_client_names, linter)
+          end
+        end
+      elseif type(ft_v) == "string" then
+        if buf_ft == ft_k then
+          table.insert(buf_client_names, ft_v)
+        end
+      end
+    end
+  end
+
+  local ok, conform = pcall(require, "conform")
+  local formatters = table.concat(conform.list_formatters_for_buffer(), " ")
+  if ok then
+    for formatter in formatters:gmatch "%w+" do
+      if formatter then
+        table.insert(buf_client_names, formatter)
+      end
+    end
+  end
+
+  local hash = {}
+  local unique_client_names = {}
+
+  for _, v in ipairs(buf_client_names) do
+    if not hash[v] then
+      unique_client_names[#unique_client_names + 1] = v
+      hash[v] = true
+    end
+  end
+  local language_servers = table.concat(unique_client_names, ", ")
+
+  return "" --.. language_servers
+end
+
+local macro = {
+  require("noice").api.status.mode.get,
+  cond = require("noice").api.status.mode.has,
+  color = { fg = colors.red, bg = colors.bg_dark, gui = "italic,bold" },
+}
+
+local dia = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  symbols = { error = " ", warn = " ", info = " ", hint = " " },
+  diagnostics_color = {
+    error = { fg = colors.red },
+    warn = { fg = colors.yellow },
+    info = { fg = colors.purple },
+    hint = { fg = colors.cyan },
+  },
+  color = { bg = colors.gray2, fg = colors.blue, gui = "bold" },
+  separator = { left = "", right = "" },
+}
+
+local lsp = {
+  function()
+    return getLspName()
+  end,
+  separator = { left = "", right = "" },
+  color = { bg = colors.purple, fg = colors.bg, gui = "italic,bold" },
+}
+
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- fancy icons
+    "linrongbin16/lsp-progress.nvim", -- LSP loading progress
+  },
+  opts = {
+    options = {
+      disabled_filetypes = {
+        statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" },
+      },
+      icons_enabled = true,
+      theme = theme,
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = true,
+    },
+
+    sections = {
+      lualine_a = {
+        modes,
+      },
+      lualine_b = {
+        space,
+        filename,
+        -- filetype,
+      },
+      lualine_c = {
+        "%=",
+        dia,
+      },
+      lualine_x = {},
+      lualine_y = {
+        -- macro,
+        -- space
+      },
+      lualine_z = {
+        diff,
+        branch,
+        lsp,
+      },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+  },
+}
